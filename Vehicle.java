cfeeafedda1ede5b53567f51b5893bc6506b6da5
Pashatania Fitri Indah Lestari <87978506+pashatania1@users.noreply.github.com>
2025-09-09 11:46:00 +0700

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle {
+    //property/atribut/variabel
+    private String make;
+    private int milesPerGallon;
+
+    //Constructor
+    public Vehicle() {
+        make = "BMW";
+        milesPerGallon = 50;
+    }
+}
---
+++ VehicleTest.java
@@ -0,0 +1,6 @@
+public class VehicleTest {
+    public static void main(String[] args) {
+        Vehicle car = new Vehicle();
+        Vehicle bus = new Vehicle();
+    }
+}
